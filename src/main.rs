1b35582793d02c96a4115e7dce51959cdeacc001
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-07-16 18:47:59 -0400

    Printing memory information

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn print_mac_specs() {
     print_operating_system_information();
     println!();
     print_disk_information();
+    println!();
+    print_memory_information();
 }
 
 fn print_user_information() {
@@ -34,6 +36,16 @@ fn print_disk_information() {
 
     let space_used = (total_disk_space - free_disk_space) as f64 / total_disk_space as f64;
     println!("Disk Space Used: {:.2}%", space_used * 100 as f64);
+
+fn print_memory_information() {
+    let free_memory_space = sys_info::mem_info().unwrap().free;
+    let total_memory_space = sys_info::mem_info().unwrap().total;
+
+    println!("Memory Free: {}", free_memory_space);
+    println!("Memory Total: {}", total_memory_space);
+
+    let space_used = (total_memory_space - free_memory_space) as f64 / total_memory_space as f64;
+    println!("Memory Used: {:.2}%", space_used * 100 as f64);
 }
 
 fn bytes_to_gigabytes (bytes: u64) -> u64 {
