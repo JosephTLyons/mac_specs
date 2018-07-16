15ef9a26177aea5057eb5cc9622ff7439638873a
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-07-16 18:32:06 -0400

    Now printing username and more OS information

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 extern crate os_info;
+extern crate os_type;
 extern crate sys_info;
 extern crate users;
 
 fn print_mac_specs() {
-    //println!("Username: {}", username::)
+    print_user();
     println!("Hostname: {}", sys_info::hostname().unwrap());
+    println!();
     print_operating_system_information();
+    println!();
     print_disk_information();
 }
 
+fn print_user() {
+    use users::{get_user_by_uid, get_current_uid};
+    let user = get_user_by_uid(get_current_uid()).unwrap();
+    println!("User: {}", user.name());
+}
+
 fn print_operating_system_information() {
+    println!("Operating System: {:?}", os_type::current_platform().os_type);
+    println!("Operating System Version: {:?}", os_type::current_platform().version);
     println!("Operating System Type: {}", sys_info::os_type().unwrap());
-    println!("Operating System: {}", sys_info::os_release().unwrap());
 }
 
 fn print_disk_information() {
     let free_space = sys_info::disk_info().unwrap().free;
     let total_space = sys_info::disk_info().unwrap().total;
 
-    // println!("{}", sys_info::disk_info().unwrap().free);
-    //
-    // println!("Disk Space Free: {}", bytes_to_gigabytes (free_space));
-    // println!("Disk Space Total: {}", bytes_to_gigabytes (total_space));
+    //println!("Disk Space Free: {}", bytes_to_gigabytes (free_space));
+    //println!("Disk Space Total: {}", bytes_to_gigabytes (total_space));
 
     let space_used = (total_space - free_space) as f64 / total_space as f64;
     println!("Disk Space Used: {:.2}%", space_used * 100 as f64);
